192e88f251e9a9717e1a7ec1c396036dc2cd19ee
Stas_Krasnagir <stas220693@gmail.com>
2021-01-10 17:18:56 +0300

    09-01-21

--- snake.py
+++ snake.py
@@ -3,23 +3,27 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 move_increment = 20
-moves_per_second = 1
-game_speed = 1000 // moves_per_second
-g_width = 600
-g_height = 620
+
 
 class Snake(tk.Canvas):
-    def __init__(self):
-        super().__init__(width=g_width, height=g_height, background="black", highlightthickness=0)
+    def __init__(self, width, height):
+        super().__init__(width=width, height=height, background="black", highlightthickness=0)
+        self.width = width
+        self.height = height
+
         self.snake_positions = [(100, 100), (80, 100), (60, 100)]
+
         self.food_position = self.set_new_food_position()
         self.bonus_position = self.set_new_bonus_position()
+        self.bonus_food_position = self.set_new_bonus_food_position()
+
         self.score = 0
+        self.game_speed = 150
         self.directions = "Right"
         self.bind_all("<Key>", self.on_key_press)
         self.load_assets()
         self.create_objects()
-        self.after(game_speed, self.perform_actions)
+        self.after(self.game_speed, self.perform_actions)
 
     def load_assets(self):
         try:
@@ -29,29 +33,30 @@ class Snake(tk.Canvas):
             self.food_image = Image.open("./assets/food.png")
             self.food = ImageTk.PhotoImage(self.food_image)
 
-            self.bonus_image = Image.open("./assets/bonus.png")
-            self.bonus = ImageTk.PhotoImage(self.bonus_image)
+            self.bonus_image = Image.open("./assets/bonus_speed.png")
+            self.bonus_speed = ImageTk.PhotoImage(self.bonus_image)
+
+            self.bonus_food_image = Image.open("./assets/bonus_food.png")
+            self.bonus_food = ImageTk.PhotoImage(self.bonus_food_image)
 
         except IOError as error:
-            global root
             root.destroy()
             raise
 
     def create_objects(self):
         self.create_text(
-            100, 12, text=f"Score {self.score} (speed: {moves_per_second})",
+            100, 12, text=f"Score {self.score} (speed: {self.game_speed})",
             tag="score", fill="#fff", font=("TkDefaultFront", 14))
 
         for x_position, y_position in self.snake_positions:
             self.create_image(x_position, y_position, image=self.snake_body, tag="snake")
 
         self.create_image(*self.food_position, image=self.food, tag="food")
-        self.create_image(*self.bonus_position, image=self.bonus, tag="bonus")
+        self.create_image(*self.bonus_position, image=self.bonus_speed, tag="bonus")
+        self.create_image(*self.bonus_food_position, image=self.bonus_food, tag="bonus_food")
 
     def move_snake(self):
-        global new_head_position
         head_x_position, head_y_position = self.snake_positions[0]
-
         if self.directions == "Left":
             new_head_position = (head_x_position - move_increment, head_y_position)
         elif self.directions == "Right":
@@ -73,12 +78,13 @@ class Snake(tk.Canvas):
         self.move_snake()
         self.check_food_collision()
         self.check_bonus_collision()
-        self.after(game_speed, self.perform_actions)
+        self.check_bonus_food_collision()
+        self.after(self.game_speed, self.perform_actions)
 
     def check_collisions(self):
         head_x_position, head_y_position = self.snake_positions[0]
-        return (head_x_position in (0, g_width)
-                or head_y_position in (20, g_height)
+        return (head_x_position in (0, self.width)
+                or head_y_position in (20, self.height)
                 or (head_x_position, head_y_position) in self.snake_positions[1:])
 
     def on_key_press(self, e):
@@ -95,26 +101,17 @@ class Snake(tk.Canvas):
             self.create_image(*self.snake_positions[-1], image=self.snake_body, tag="snake")
 
             if self.score % 1 == 5:
-                global moves_per_second
-                moves_per_second += 1
-
-            if self.score % 7 == 0:
-                rate_grow = 0
-                while rate_grow < 7:
-                    self.snake_positions.append(self.snake_positions[-1])
-                    self.create_image(*self.snake_positions[-1], image=self.snake_body, tag="snake")
-                    rate_grow += 1
-                    self.score += 1
+                self.game_speed -= 1
 
             self.food_position = self.set_new_food_position()
             self.coords(self.find_withtag("food"), self.food_position)
 
             score = self.find_withtag("score")
-            self.itemconfigure(score, text=f"Score: {self.score} (speed:{moves_per_second})", tag="score")
+            self.itemconfigure(score, text=f"Score: {self.score} (speed:{self.game_speed})", tag="score")
 
     def set_new_food_position(self):
-        x_position = randint(1, (g_width // 20) - 1) * move_increment
-        y_position = randint(3, (g_height // 20) - 1) * move_increment
+        x_position = randint(1, (self.width // 20) - 1) * move_increment
+        y_position = randint(3, (self.height // 20) - 1) * move_increment
         food_position = (x_position, y_position)
 
         if food_position not in self.snake_positions:
@@ -122,21 +119,43 @@ class Snake(tk.Canvas):
 
     def check_bonus_collision(self):
         if self.snake_positions[0] == self.bonus_position:
-            global moves_per_second
-            moves_per_second += 10
-
+            self.game_speed -= 25
             self.bonus_position = self.set_new_bonus_position()
             self.coords(self.find_withtag("bonus"), self.bonus_position)
 
             score = self.find_withtag("score")
-            self.itemconfigure(score, text=f"Score: {self.score} (speed:{moves_per_second})", tag="score")
+            self.itemconfigure(score, text=f"Score: {self.score} (speed:{self.game_speed})", tag="score")
 
     def set_new_bonus_position(self):
-        x_position_b = randint(1, (g_width // 20) - 1) * move_increment
-        y_position_b = randint(3, (g_height // 20) - 1) * move_increment
+        x_position_b = randint(1, (self.width // 20) - 1) * move_increment
+        y_position_b = randint(3, (self.height // 20) - 1) * move_increment
         bonus_position = (x_position_b, y_position_b)
         if bonus_position not in self.snake_positions and bonus_position not in self.food_position:
             return bonus_position
+
+    def check_bonus_food_collision(self):
+        if self.snake_positions[0] == self.bonus_food_position:
+            rate_grow = 0
+            while rate_grow < 10:
+                self.snake_positions.append(self.snake_positions[-1])
+                self.create_image(*self.snake_positions[-1], image=self.snake_body, tag="snake")
+                rate_grow += 1
+                self.score += 2
+            rate_grow = 0
+            self.bonus_food_position = self.set_new_bonus_position()
+
+            self.coords(self.find_withtag("bonus_food"), self.bonus_food_position)
+            score = self.find_withtag("score")
+            self.itemconfigure(score, text=f"Score: {self.score} (speed:{self.game_speed})", tag="score")
+
+    def set_new_bonus_food_position(self):
+        x_position_bf = randint(1, (self.width // 20) - 1) * move_increment
+        y_position_bf = randint(3, (self.height // 20) - 1) * move_increment
+        bonus_food_position = (x_position_bf, y_position_bf)
+        if bonus_food_position not in self.snake_positions \
+                and bonus_food_position not in self.food_position\
+                and bonus_food_position not in self.bonus_position:
+            return bonus_food_position
 
     def end_game(self):
         self.delete(tk.ALL)
@@ -154,19 +173,18 @@ root.resizable(False, False)
 
 
 def start_snake():
-    global g_width, g_height
-    g_width = int(text1.get())
-    g_height = int(text2.get())
-    if g_width < 300:
-        g_width = 300
-    if g_height < 320:
-        g_height = 320
+    width = int(text1.get())
+    height = int(text2.get())
+    if width < 300:
+        width = 300
+    if height < 320:
+        height = 320
     width_lable.grid_forget()
     text1.grid_forget()
     height_lable.grid_forget()
     text2.grid_forget()
     but.grid_forget()
-    board = Snake()
+    board = Snake(width, height)
     board.grid()
 
 
